bec170633c884204943eb6cdfe82d199977b2322
Hercilio Tenorio <hercilio.tenorio@ufrpe.br>
2023-12-12 10:29:37 -0300

    Correção HOST

--- main.py
+++ main.py
@@ -37,4 +37,4 @@ async def success_aluno(request: Request):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, log_level='info', reload=True)
+    uvicorn.run(app="main:app", host="127.0.0.1", port=8000, log_level='info', reload=True)
